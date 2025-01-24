3178e08de6a59ea49dffa924553fee1a35cc50f3
Atanas Donev <a.hristov.donev@gmail.com>
2025-01-24 15:52:16 +0100

    Adding basic bearer token authentication

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use actix_web::{
-    App, HttpServer
-};
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
+use crate::endpoints::authentication::{login_handler, protected_resource_handler};
 
 static ROOT_DIR: &str = "./root";
 
 mod endpoints;
 mod utilities;
+mod services;
+mod models;
 
 use crate::endpoints::download::download_file_from_root_directory;
 use crate::endpoints::download::download_file_from_user_directory;
 use crate::endpoints::upload::upload_file_from_root_directory;
 use crate::endpoints::upload::upload_file_from_user_directory;
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Ensure the upload directory exists
     std::fs::create_dir_all(ROOT_DIR)?;
@@ -21,6 +23,10 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
+            .wrap(models::auth_models::JwtAuth)
+            .service(login_handler)
+            .service(web::resource("/protected").route(web::get().to(protected_resource_handler)))
             .service(download_file_from_root_directory)
             .service(download_file_from_user_directory)
             .service(upload_file_from_root_directory)
