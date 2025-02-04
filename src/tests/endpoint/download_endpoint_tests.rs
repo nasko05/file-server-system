7c5649e737b98929f7c4c466addcbe643dd9e7f8
Atanas Donev <a.hristov.donev@gmail.com>
2025-02-04 18:08:59 +0100

    Adding download tests

--- src/tests/endpoint/download_endpoint_tests.rs
+++ src/tests/endpoint/download_endpoint_tests.rs
@@ -60,4 +60,47 @@ mod tests {
         let expected_msg = "Some text!";
         assert_eq!(std::str::from_utf8(&body).unwrap(), expected_msg);
     }
+
+    #[actix_web::test]
+    async fn test_download_file_not_found() {
+        let env = get_global_test_env().await;
+        let test_root = env.root_dir.path();
+        let username = "test_user";
+        let user_dir = test_root.join(username);
+        let sub_path = "";
+        let file_dir = user_dir.join(sub_path);
+        let file_to_download = "test.txt";
+
+        fs::create_dir_all(&file_dir).expect("failed to create files directory");
+        // Ensure the directory exists
+        let target_file = user_dir.join(sub_path).join(file_to_download);
+
+        let payload = DownloadFileRequest {
+            filename: file_to_download.to_string(),
+            path: sub_path.to_string(),
+        };
+
+        let token = generate_jwt("test_user".to_string()).expect("failed to generate token");
+        let req = test::TestRequest::post()
+            .uri("/download")
+            .insert_header((AUTHORIZATION, format!("Bearer {}", token)))
+            .set_json(&payload)
+            .to_request();
+
+        let config = AppConfig {
+            root_dir: Arc::new(test_root.to_str().unwrap().to_string())
+        };
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(config))
+                .wrap(JwtAuth)
+                .service(download_file_from_user_directory)
+        ).await;
+
+        let resp = test::call_service(&app, req).await;
+
+        println!("{}", resp.status());
+        assert_eq!(resp.status(), 400);
+        assert!(!target_file.exists());
+    }
 }
